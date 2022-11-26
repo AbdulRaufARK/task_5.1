23c324c5c96f49d08ce77c78946b9be0c3751902
Abdul Rauf <40770051+AbdulRaufARK@users.noreply.github.com>
2022-11-26 18:01:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+    var strtext1 = document.getElementById("Text1");
+    var strtext2 = document.getElementById("Text2");
+    var strtext3 = document.getElementById("Text3");
+    var strtext4 = document.getElementById("Text4");
+function getData(){
+
+    var a = strtext1.value;
+    var b = strtext2.value;
+    var c = strtext3.value; 
+    var d = strtext4.value; 
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+
+    
+
+}
